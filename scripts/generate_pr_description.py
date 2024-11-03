f40fdff5f3a69c2ab77a6cc3d4745f88e688c922
Takeru O'oyama <tqer39@gmail.com>
2024-11-03 09:18:06 +0900

    Feature/v0.0.17 alpha 20241103 (#21)
    * Feature: Replace decimal.Decimal with float for temperature input in generate_pr_description.py
    
    * Feature: Update generate-pr-description-action to v0.0.16-alpha and specify model as "gpt-4o"

--- scripts/generate_pr_description.py
+++ scripts/generate_pr_description.py
@@ -4,7 +4,6 @@
 #     "openai",
 # ]
 # ///
-import decimal
 from openai import OpenAI
 import os
 import subprocess
@@ -82,7 +81,7 @@ def generate_pr_description(commit_logs: str) -> str:
             {"role": "user", "content": prompt},
         ],
         max_tokens=1000,
-        temperature=decimal.Decimal(os.getenv("TEMPERATURE") or 0.7),
+        temperature=float(os.getenv("TEMPERATURE", "0.7")),
     )
 
     return str(response.choices[0].message.content).strip()
